c9435318f961dd5b7300184b4c0a5ed4f345cd17
Ryan Hazzard <rhazzardphoto@gmail.com>
2019-06-22 18:51:26 -0700

    done, could style more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,41 @@
 //arr of objects with all aspects of question 
 var all = [{
     question: "What Native American language was Super Bowl XXX the first to be broadcast in?",
-    answer: ["a. beer", "b. Navajo", "c. beer", "d. beer"],
-    explanation: "Nav",
-    picture: '../images/DSC_3981.jpg'
+    answer: ["a. Spanish", "b. Navajo", "c. French", "d. Yupik"],
+    explanation: "Navajo",
+    picture: './assets/images/navajo.jpg'
 },
 {
     question: "How many years must a player be retired to be eligible for the Pro Football Hall of Fame?",
-    answer: ["a. beer", "b. beer", "c. five", "d. beer"],
-    explanation: "Beer",
-    picture: '../images/DSC_3981.jpg'
+    answer: ["a. ten", "b. seven", "c. five", "d. one"],
+    explanation: "five",
+    picture: './assets/images/download.png'
 },
 {
     question: "What was Miami quarterback Bob Griese the first NFL football player to wear in a game, in 1977?",
-    answer: ["a. Glasses", "b. beer", "c. beer", "d. beer"],
+    answer: ["a. Glasses", "b. sleeves", "c. helmet", "d. rainbow cleats"],
     explanation: "Glasses",
-    picture: '../images/DSC_3981.jpg'
+    picture: './assets/images/bob.jpg'
 },
 {
     question: "What record-setting quarterback was the NFL's 82nd draft pick in 1979?",
-    answer: ["a. beer", "b. beer", "c. beer", "d. Joe Montana"],
-    explanation: "Joe",
-    picture: '../images/DSC_3981.jpg'
+    answer: ["a. Peyton Manning", "b. Aaron Rodgers", "c. Troy Aikman", "d. Joe Montana"],
+    explanation: "Joe Montana",
+    picture: './assets/images/Montana-Joe-2.jpg'
 }];
 
 var correctAnswer = ["b. Navajo", "c. five", "a. Glasses", "d. Joe Montana"]
+var approval = ["Nice", "You're a trivia pro", "Good Work", "A++"]
+var disapproval = ["Not quite", "c'mon son", "bruh", "You're better than that"]
+function approve () {
+    goodJob = approval[Math.floor(Math.random()*approval.length)];
+    return goodJob
+}
+function disapprove () {
+    badJob = disapproval[Math.floor(Math.random()*disapproval.length)];
+    return badJob
+}
 var timer = 10;
-$('#timer').html(10)
 var right = 0;
 var wrong = 0;
 var i = -1;
@@ -45,23 +54,26 @@ function time() {
     } else if (timer === 0) {
         display(i)
     } else if (i >= all.length)
-    gameOver(start); 
+        gameOver(start);
 }
 //on click start 
 $('#start').on('click', function () {
+    $('#start').hide()
     start = setInterval(time, 1000);
     display(i)
 })
-
 //writes question and answer 
 function display() {
+    $('#phrase').html('Good Luck!')
     i++
+    $('#timer').show()
+    $('#timer').html(10)
     console.log(i)
     timer = 10
     $('#question').html(all[i].question)
     $('#answer').html('')
     for (var n = 0; n < all[i].answer.length; n++) {
-        $('#answer').append(`<button class="answer" data-attribute="${all[i].answer[n]}" id="${all[i].answer[n]}">${all[i].answer[n]}</button>`)
+        $('#answer').append(`<div class="answer" data-attribute="${all[i].answer[n]}" id="${all[i].answer[n]}">${all[i].answer[n]}</div>`)
     }
 }
 //on click for answers 
@@ -70,30 +82,31 @@ $('#answer').on('click', function (event) {
     console.log(click)
     if (click === correctAnswer[i]) {
         console.log('true')
+        $('#phrase').html(approve())
         right++;
         transition(i)
         setTimeout(display, 4000)
     } else {
         console.log('false')
+        $('#phrase').html(disapprove())
         wrong++;
         transition(i)
         setTimeout(display, 4000)
     }
 })
-
 //display correct answer, picture, description, right or wrong
 function transition(i) {
     timer = 5;
-    $('#timer').html('Answer')
+    $('#timer').hide()
     $('#question').html(`<p>${all[i].explanation}</p>`)
-    $('#answer').html(`<img url="${all[i].picture}">`)
+    $('#answer').html(`<img id="picture" src="${all[i].picture}">`)
+    console.log(`${all[i].picture}`)
     console.log(`right: ${right}, wrong: ${wrong}`)
 }
-
-
 //end of game 
-function gameOver (clear) {
+function gameOver(clear) {
     clearInterval(clear)
+    $('#phrase').html('')
     $('#timer').html('')
     $('#question').html(`<p>right: ${right}</p>`)
     $('#answer').html(`<p>wrong: ${wrong}</p>`)
